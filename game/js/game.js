a035ed6bffee6662b487098276eda98fa1fab8af
Frankoslaw <franopusz2006@gmail.com>
2019-05-27 18:07:42 +0200

    dalsze prace nad tilemapą

--- game/js/game.js
+++ game/js/game.js
@@ -92,70 +92,40 @@ var game = new Phaser.Game(config);
 
 "use strict";
 
-var config = {
-    type: Phaser.CANVAS,
-    width: 400,
-    height: 300,
-    backgroundColor: '#2d2d2d',
-    parent: 'phaser-example',
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-    pixelArt: true,
-
-    physics: {
-        default: 'arcade',
-        arcade: {
-            debug: false
-        }
-    },
-
+const config = {
+    type: Phaser.AUTO, // Which renderer to use
+    width: 800, // Canvas width in pixels
+    height: 600, // Canvas height in pixels
+    parent: "game-container", // ID of the DOM element to add the canvas to
     scene: {
-        preload: preload,
-        create: create,
-        update: update
+      preload: preload,
+      create: create,
+      update: update
     }
-};
-
-var controls;
-
-var game = new Phaser.Game(config);
-
-function preload ()
-{
-    this.load.image('playerimg', 'https://frankoslaw.github.io/who-is-alien/game/assets/images/player.png',{ frameWidth: 16, frameHeight: 16 });
-    this.load.image("button", "https://frankoslaw.github.io/who-is-alien/game/assets/images/button.png",{ frameWidth: 15, frameHeight: 15 });
-
-    this.load.image("tiles1", "https://frankoslaw.github.io/who-is-alien/game/assets/images/tilemap.png");
-    this.load.tilemapTiledJSON("map1", "https://frankoslaw.github.io/who-is-alien/game/assets/mapa.json");
-}
-
-function create ()
-{
-    var map1 = this.make.tilemap({ key: 'map1' });
-    var tileset1 = map1.addTilesetImage('Game map 1', 'tiles1',63,63);
-
-    var layer1 = map1.createStaticLayer('World1', tileset1, 63, 63);
-    //var layer2 = map1.createStaticLayer('World1', tileset1, 0, 0);
-
-
-    var cursors = this.input.keyboard.createCursorKeys();
-
-    var controlConfig = {
-        camera: this.cameras.main,
-        left: cursors.left,
-        right: cursors.right,
-        speed: 0.5
-    };
-
-    controls = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig);
-
-    this.cameras.main.setBounds(0, 0,layer1.x + layer1.width, 0);
-}
-
-function update (time, delta)
-{
-    controls.update(delta);
-}
+  };
+  
+  const game = new Phaser.Game(config);
+  
+  function preload() {
+    this.load.image("tiles", "https://frankoslaw.github.io/who-is-alien/game/assets/images/tilemap.png");
+    this.load.tilemapTiledJSON("map", "https://frankoslaw.github.io/who-is-alien/game/assets/mapa0.0.1.json");
+  }
+  
+  function create() {
+    const map = this.make.tilemap({ key: "map" });
+  
+    // Parameters are the name you gave the tileset in Tiled and then the key of the tileset image in
+    // Phaser's cache (i.e. the name you used in preload)
+    const tileset = map.addTilesetImage("tuxmon-sample-32px-extruded", "tiles");
+  
+    // Parameters: layer name (or index) from Tiled, tileset, x, y
+    const walls = map.createStaticLayer("walls", tileset, 0, 0);
+    const ground = map.createStaticLayer("ground", tileset, 0, 0);
+  }
+  
+  function update(time, delta) {
+    // Runs once per frame for the duration of the scene
+  }
 
 
 

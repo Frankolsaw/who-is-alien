fce8d87974cbd01b9db4c6f7be4aadc297583d48
Frankoslaw <franopusz2006@gmail.com>
2019-05-27 19:50:32 +0200

    new camera position

--- game/js/game.js
+++ game/js/game.js
@@ -74,8 +74,9 @@
     });
     camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
   
-    camera.x = 1890;
-    camera.y = 1134;
+    //camera.x = 1890;
+    //camera.y = 1134;
+    camera.setPosition(1890, 1134);
 
     player = this.physics.add.sprite(200, 150, 'playerimg').setScrollFactor(0);
 
